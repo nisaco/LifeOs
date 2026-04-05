2f0e93c761b01c12de8e44128096f6db3b6f997a
Nii <jnkpappoe@gmail.com>
2026-04-05 19:22:26 +0000

    Final fix for Express 5 catch-all route

--- server.js
+++ server.js
@@ -272,7 +272,10 @@ app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'favicon.ico'));
 });
 
-app.get('.*', (req, res) => { // Use wildcard to support SPA refreshes
+//--app.get('.*', (req, res) => { // Use wildcard to support SPA refreshes
+  //res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+//});
+app.get('/:any*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
